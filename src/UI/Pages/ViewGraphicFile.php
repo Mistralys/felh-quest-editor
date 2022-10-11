b73ed4aaef19c9918129770cb405182d40c4df14
Mistralys <s.mordziol@gmail.com>
2022-10-11 21:45:44 +0200

    Added an image viewer.

---
+++ src/UI/Pages/ViewGraphicFile.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @package FELH Quest Editor
+ * @subpackage UI Pages
+ * @see \Mistralys\FELHQuestEditor\UI\Pages\ViewGraphicFile
+ */
+
+declare(strict_types=1);
+
+namespace Mistralys\FELHQuestEditor\UI\Pages;
+
+use AppUtils\FileHelper;
+use AppUtils\FileHelper\FileInfo;
+use AppUtils\FileHelper_Exception;
+use Mistralys\FELHQuestEditor\UI\BasePage;
+
+/**
+ * Image viewer to display an image from a game folder.
+ *
+ * @package FELH Quest Editor
+ * @subpackage UI Pages
+ */
+class ViewGraphicFile extends BasePage
+{
+    public const URL_NAME = 'view-graphic-file';
+
+    public function getTitle() : string
+    {
+        return 'View graphic file';
+    }
+
+    public function display() : void
+    {
+        $path = FileInfo::factory(FELHQM_GAME_FOLDER.'/'.$this->request->getParam('target'));
+
+        if($path->exists() && in_array($path->getExtension(), array('png', 'jpg'), true)) {
+            $this->sendFile($path->getPath());
+        }
+
+        $this->sendFile(__DIR__.'/../../../img/broken-image.jpg');
+    }
+
+    /**
+     * @param string $path
+     * @return never
+     * @throws FileHelper_Exception
+     */
+    private function sendFile(string $path) : void
+    {
+        $mime = FileHelper::detectMimeType($path);
+        header('Content-Type: '.$mime);
+
+        readfile($path);
+        exit;
+    }
+}
