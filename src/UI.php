580a02e2e1eda2439be2405682884edff41dd763
Mistralys <s.mordziol@gmail.com>
2022-10-11 21:46:55 +0200

    Added `addJSOnload()`.

--- src/UI.php
+++ src/UI.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace Mistralys\FELHQuestEditor;
 
 use Mistralys\FELHQuestEditor\UI\Icon;
+use newrelic\DistributedTracePayload;
 
 class UI
 {
     private static bool $showXMLTags = false;
+
+    /**
+     * @var string[]
+     */
+    private static array $jsAutoLoad = array();
 
     public static function icon(string $id='') : Icon
     {
@@ -23,5 +29,17 @@ class UI
     public static function isShowXMLTagsEnabled() : bool
     {
         return self::$showXMLTags;
+    }
+
+    public static function addJSOnload(string $statement) : void
+    {
+        if(!in_array($statement, self::$jsAutoLoad)) {
+            self::$jsAutoLoad[] = $statement;
+        }
+    }
+
+    public static function getJSAutoLoad() : array
+    {
+        return self::$jsAutoLoad;
     }
 }
