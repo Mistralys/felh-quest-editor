ae90b41aacac7651197f4fa09476e85db76bb3c7
Mistralys <s.mordziol@gmail.com>
2022-10-11 21:47:05 +0200

    Added a constructor.

--- src/UI/BasePage.php
+++ src/UI/BasePage.php
@@ -5,9 +5,17 @@ declare(strict_types=1);
 namespace Mistralys\FELHQuestEditor\UI;
 
 use AppUtils\OutputBuffering;
+use AppUtils\Request;
 
 abstract class BasePage
 {
+    protected Request $request;
+
+    public function __construct()
+    {
+        $this->request = Request::getInstance();
+    }
+
     abstract public function getTitle() : string;
 
     abstract public function display() : void;
