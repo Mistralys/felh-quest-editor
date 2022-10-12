68fbcf0b16412675d82d836fd9b656df47eadcae
Mistralys <s.mordziol@gmail.com>
2022-10-12 09:07:45 +0200

    Added treasure records.

--- src/Quest.php
+++ src/Quest.php
@@ -11,23 +11,32 @@ use AppUtils\Request;
 use Mistralys\FELHQuestEditor\AttributeHandling\Attributes\EnumAttribute;
 use Mistralys\FELHQuestEditor\AttributeHandling\BaseRecord;
 use Mistralys\FELHQuestEditor\AttributeHandling\RecordGroup;
+use Mistralys\FELHQuestEditor\CommonRecords\TreasureContainerInterface;
+use Mistralys\FELHQuestEditor\CommonRecords\TreasureContainerTrait;
+use Mistralys\FELHQuestEditor\DataTypes\TacticalMapLocations;
 use Mistralys\FELHQuestEditor\UI\Pages\EditQuest;
+use function AppLocalize\t;
 use function AppUtils\sb;
 
-class Quest extends BaseRecord
+class Quest extends BaseRecord implements TreasureContainerInterface
 {
+    use TreasureContainerTrait;
+
     public const TAG_TACTICAL_MAP_ID = 'PrefQuestLoc';
     public const TAG_DESCRIPTION = 'Description';
     public const TAG_DISPLAY_NAME = 'DisplayName';
     public const TAG_SHORT_TEXT_ACCEPT = 'ShortTextAccept';
+    public const TAG_SHORT_TEXT_DENY = 'ShortTextDeny';
     public const TAG_QUEST_CLASS = 'QuestClass';
+    public const TAG_REWARD_TEXT = 'RewardText';
 
     /**
      * @var QuestObjective[]
      */
     private array $objectives = array();
     private FileInfo $sourceFile;
-    private RecordGroup $objectivesAttribute;
+    private RecordGroup $objectivesGroup;
+    private RecordGroup $treasureGroup;
 
     public function __construct(ArrayDataCollection $attribs, FileInfo $sourceFile)
     {
@@ -39,103 +48,81 @@ class Quest extends BaseRecord
     protected function registerAttributes() : void
     {
         $texts = $this->attributeManager
-            ->addGroup('texts', 'Texts')
+            ->addGroup('texts', t('Texts'))
             ->setIcon(UI::icon()->texts());
 
-        $texts->registerString(self::TAG_DISPLAY_NAME, 'Display name')
+        $texts->registerString(self::TAG_DISPLAY_NAME, t('Display name'))
             ->setRequired();
 
         $texts->registerMultiLineString(self::TAG_DESCRIPTION, self::TAG_DESCRIPTION)
             ->setRequired();
 
-        $texts->registerString(self::TAG_SHORT_TEXT_ACCEPT, 'Accept text')
+        $texts->registerString(self::TAG_SHORT_TEXT_ACCEPT, t('Accept text'))
             ->setRequired();
 
+        $texts->registerString(self::TAG_SHORT_TEXT_DENY, t('Deny text'));
+
+        $texts->registerString(self::TAG_REWARD_TEXT, t('Reward text'));
+
         $general = $this->attributeManager
-            ->addGroup('settings', 'Settings')
+            ->addGroup('settings', t('Settings'))
             ->setIcon(UI::icon()->settings());
 
-        $general->registerEnum(
-            self::TAG_QUEST_CLASS,
-            'Quest class',
-            array(
-                'Minor' => 'Minor',
-                'Major' => 'Major'
-            )
-        );
+        $general->registerEnum(self::TAG_QUEST_CLASS, t('Quest class'))
+            ->addEnumItem('Minor', t('Minor quest'))
+            ->addEnumItem('Major', t('Major quest'));
 
-        $general->registerEnum(
-            'SpawnRating',
-            'Spawn rating',
-            array(
-                '1' => '1 - Trivial quests, e.g. Bacco',
-                '2' => '2 - ',
-                '3' => '3 - Substantial',
-                '4' => '4 - Major',
-                '5' => '5 - Essential, e.g. the quest of mastery'
-            )
-        )
+        $general->registerEnum('SpawnRating', t('Spawn rating'))
+            ->addEnumItem('1', '1 - Trivial quests, e.g. Bacco')
+            ->addEnumItem('2', '2 - ')
+            ->addEnumItem('3', '3 - Substantial')
+            ->addEnumItem('4', '4 - Major')
+            ->addEnumItem('5', '5 - Essential, e.g. the quest of mastery')
             ->setDescription('Sets the importance of the quest.')
             ->setRequired();
 
-        $general->registerBool('IsStartingPointQuest', 'Is starting point?')
+        $general->registerBool('IsStartingPointQuest', t('Is starting point?'))
             ->setRequired();
 
-        $general->registerBool('AllowQuestRejection', 'Can be rejected?')
+        $general->registerBool('AllowQuestRejection', t('Can be rejected?'))
             ->setRequired();
 
-        $general->registerBool('AICanGoOnQuest', 'AI compatible?')
+        $general->registerBool('AICanGoOnQuest', t('AI compatible?'))
             ->setDefault('0')
-            ->setDescription('Whether the AI players can go on this quest.');
+            ->setDescription(t('Whether the AI players can go on this quest.'));
 
-        $general->registerBool('Repeatable', 'Is repeatable?')
+        $general->registerBool('Repeatable', t('Is repeatable?'))
             ->setRequired()
-            ->setDescription('Whether the quest can happen several times in the same game.');
+            ->setDescription(t('Whether the quest can happen several times in the same game.'));
 
-        $general->registerEnum(
-            self::TAG_TACTICAL_MAP_ID,
-            'Tactical map',
-            array(
-                'QuestLoc_Ruin' => 'Ruin',
-                'QuestLoc_Inn1' => 'Inn 1',
-                'QuestLoc_Inn2' => 'Inn 2',
-                'QuestLoc_Graveyard' => 'Graveyard',
-                'QuestLoc_Palisade' => 'Palisade',
-                'QuestLoc_Utheron' => 'Utheron',
-                'QuestLoc_RefugeeCamp' => 'Refugee camp',
-                'QuestLoc_Hut' => 'Hut',
-                'QuestLoc_Camp' => 'Camp',
-                'QuestLoc_StoneRing' => 'Stone ring',
-                'QuestLoc_Cave' => 'Cave',
-                'QuestLoc_Shard' => 'Shard',
-                'QuestLoc_Dungeon' => 'Dungeon',
-                'QuestLoc_Arena' => 'Arena',
-                'QuestLoc_Gate1' => 'Gate 1',
-                'QuestLoc_Gate2' => 'Gate 2',
-                'QuestLoc_Gate3' => 'Gate 3',
-                'QuestLoc_Dragon1' => 'Dragon 1',
-                'QuestLoc_Dragon2' => 'Dragon 2',
-                'QuestLoc_Dragon3' => 'Dragon 3'
-            )
-        )
-            ->setDescription('Selects the map to use for the tactical battle.');
+        $general->registerEnum(self::TAG_TACTICAL_MAP_ID, t('Tactical map'))
+            ->addDataCollection(TacticalMapLocations::create())
+            ->setDescription(t('Selects the map to use for the tactical battle.'));
+
+
+        // ------------------------------------------------------------
+        // GRAPHICS
+        // ------------------------------------------------------------
+
+        $graphics = $this->attributeManager
+            ->addGroup('graphics', t('Graphics'))
+            ->setIcon(UI::icon()->graphics());
+
+        $graphics->registerMedallion('RewardImage', t('Reward image'));
+
+        $graphics->registerQuestImage('Image', t('Quest image'));
 
         // ------------------------------------------------------------
         // TRIGGER
         // ------------------------------------------------------------
 
         $trigger = $this->attributeManager
-            ->addGroup('trigger', 'Trigger')
+            ->addGroup('trigger', t('Trigger'))
             ->setIcon(UI::icon()->trigger());
 
-        $trigger->registerEnum(
-            'TriggerType',
-            'Trigger type',
-            array(
-                'TurnNumber' => 'Turn number',
-                'QuestLocation' => 'Quest location'
-            )
-        );
+        $trigger->registerEnum('TriggerType', t('Trigger type'))
+            ->addEnumItem('TurnNumber', t('Turn number'))
+            ->addEnumItem('QuestLocation', t('Quest location'));
 
         // QuestLocation:
         // - Always with TriggerOrigin: EventLocation.
@@ -145,27 +132,31 @@ class Quest extends BaseRecord
         // - Sometimes with TriggerOrigin: EventLocation.
         // - With TriggerChance and PostTriggerChance
 
-        $trigger->registerEnum(
-            'TriggerOrigin',
-            'Trigger origin',
-            array(
-                'EventLocation' => 'Event location'
-            )
-        );
+        $trigger->registerEnum('TriggerOrigin', t('Trigger origin'))
+            ->addEnumItem('EventLocation', t('Event location'));
 
         // Turn number for trigger type "TurnNumber"
-        $trigger->registerString('TriggerData', 'Trigger data');
+        $trigger->registerString('TriggerData', t('Trigger data'));
 
-        $trigger->registerInt('TriggerChance', 'Trigger chance')
-            ->setDescription(sprintf('Use %1$s to ensure it will be triggered.', sb()->code('1000')));
+        $trigger->registerInt('TriggerChance', t('Trigger chance'))
+            ->setDescription(t('Use %1$s or higher to ensure it will be triggered.', sb()->code('1000')));
 
-        $trigger->registerInt('PostTriggerChance', 'Post trigger chance');
+        $trigger->registerInt('PostTriggerChance', t('Post trigger chance'));
 
-        $this->objectivesAttribute = $this->attributeManager
-            ->addRecordGroup('objectives', 'Objectives')
+        $this->objectivesGroup = $this->attributeManager
+            ->addRecordGroup('objectives', t('Objectives'))
             ->setIcon(UI::icon()->objective())
             ->setRecordsAddable('objective')
-            ->setDescription('List of objectives that can be fulfilled by this quest.');
+            ->setDescription(t('List of objectives that can be fulfilled by this quest.'));
+
+        $this->treasureGroup = $this->attributeManager
+            ->addRecordGroup('treasure', t('Treasure'))
+            ->setIcon(UI::icon()->treasure());
+    }
+
+    public function getTreasureGroup() : RecordGroup
+    {
+        return $this->treasureGroup;
     }
 
     /**
@@ -208,7 +199,7 @@ class Quest extends BaseRecord
     public function addObjective(QuestObjective $objective) : void
     {
         $this->objectives[] = $objective;
-        $this->objectivesAttribute->addRecord($objective);
+        $this->objectivesGroup->addRecord($objective);
     }
 
     public function getTacticalMapID() : string
