9f271d7f638cc05da5644135e6c2fbac88b14ff1
Mistralys <s.mordziol@gmail.com>
2022-10-16 18:50:38 +0200

    Better exception details.

--- src/UI/Pages/ExceptionPage.php
+++ src/UI/Pages/ExceptionPage.php
@@ -7,6 +7,7 @@ namespace Mistralys\FELHQuestEditor\UI\Pages;
 use AppUtils\ConvertHelper_ThrowableInfo;
 use Mistralys\FELHQuestEditor\UI\BasePage;
 use Throwable;
+use function AppLocalize\pt;
 use function AppUtils\parseThrowable;
 
 class ExceptionPage extends BasePage
@@ -29,6 +30,34 @@ class ExceptionPage extends BasePage
     {
         ?>
         <pre><?php echo $this->info->renderErrorMessage(true); ?></pre>
+        <h2>Calls</h2>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Number</th>
+                    <th class="align-right"><?php pt('Class'); ?></th>
+                    <th>File</th>
+                    <th>Function</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            $calls = $this->info->getCalls();
+
+            foreach($calls as $call)
+            {
+                ?>
+                <tr>
+                    <td><?php echo $call->getPosition() ?></td>
+                    <td class="align-right"><?php echo $call->getClass() ?></td>
+                    <td><?php echo $call->getFileName() ?>:<?php echo $call->getLine() ?></td>
+                    <td><?php echo $call->getFunction() ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+            </tbody>
+        </table>
         <?php
     }
 }
