b476f88ae3090f91f5ab6c4d41dd8517c42c1ff4
Mistralys <s.mordziol@gmail.com>
2022-10-16 18:47:59 +0200

    Added icons and sublabels.

--- src/AttributeHandling/BaseRecord.php
+++ src/AttributeHandling/BaseRecord.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Mistralys\FELHQuestEditor\AttributeHandling;
 
 use AppUtils\ArrayDataCollection;
+use Mistralys\FELHQuestEditor\UI;
+use Mistralys\FELHQuestEditor\UI\Icon;
 
 abstract class BaseRecord
 {
     protected AttributeManager $attributeManager;
     protected ArrayDataCollection $attributes;
+    protected string $subLabel = '';
 
     public function __construct(ArrayDataCollection $attribs)
     {
@@ -17,6 +20,17 @@ abstract class BaseRecord
         $this->attributeManager = new AttributeManager($this->attributes);
 
         $this->registerAttributes();
+        $this->init();
+    }
+
+    protected function init() : void
+    {
+
+    }
+
+    public function getAttributes() : ArrayDataCollection
+    {
+        return $this->attributes;
     }
 
     public function getAttributeManager() : AttributeManager
@@ -26,6 +40,12 @@ abstract class BaseRecord
 
     abstract protected function registerAttributes() : void;
     abstract public function getLabel() : string;
+    abstract public function getIcon() : ?Icon;
+
+    public function getSubLabel() : string
+    {
+        return $this->subLabel;
+    }
 
     public function getForm() : AttributeForm
     {
