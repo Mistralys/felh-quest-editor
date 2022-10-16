b451a44cdcd2decce23e41db65b756feb9b4dd12
Mistralys <s.mordziol@gmail.com>
2022-10-16 18:44:18 +0200

    Added the encounter attribute.

--- src/QuestChoice.php
+++ src/QuestChoice.php
@@ -6,22 +6,36 @@ namespace Mistralys\FELHQuestEditor;
 
 use AppUtils\ArrayDataCollection;
 use Mistralys\FELHQuestEditor\AttributeHandling\BaseRecord;
+use Mistralys\FELHQuestEditor\AttributeHandling\RecordGroup;
+use Mistralys\FELHQuestEditor\CommonRecords\Encounter;
+use Mistralys\FELHQuestEditor\UI\Icon;
+use function AppLocalize\t;
+use function AppUtils\sb;
 
 class QuestChoice extends BaseRecord
 {
     private QuestObjective $objective;
     private int $number;
+    private ?Encounter $encounter = null;
+    private RecordGroup $encounterAttribute;
 
-    public function __construct(QuestObjective $objective, int $number, ArrayDataCollection $attribs)
+    public function __construct(QuestObjective $questObjective, int $number, ArrayDataCollection $attribs)
     {
         $this->number = $number;
-        $this->objective = $objective;
+        $this->objective = $questObjective;
         parent::__construct($attribs);
+
+        $this->subLabel = $this->getDescription();
     }
 
     public function getLabel() : string
     {
-        return 'Choice #'.$this->number;
+        return t('Choice %1$s', '#'.$this->number);
+    }
+
+    public function getIcon() : ?Icon
+    {
+        return UI::icon()->choices();
     }
 
     public function getObjective() : QuestObjective
@@ -31,15 +45,35 @@ class QuestChoice extends BaseRecord
 
     protected function registerAttributes() : void
     {
-        $group = $this->attributeManager->addGroup('settings', 'Settings');
+        $group = $this->attributeManager->addGroupSettings();
 
-        $group->registerString('Description', 'Description');
+        $group->registerString('Description', 'Description')
+            ->setRequired();
 
         $group->registerInt('NextObjectiveID', 'Next objective ID');
+
+        $group->registerBool('AutoSelectChoice', t('Auto select choice'));
+
+        $this->encounterAttribute = $this->attributeManager->addRecordGroup('encounter', t('Encounter'))
+            ->setMaxRecords(1)
+            ->setMinRecords(1)
+            ->setIcon(UI::icon()->encounter());
     }
 
     public function getDescription() : string
     {
         return $this->attributes->getString('Description');
+    }
+
+    public function setEncounter(Encounter $encounter) : self
+    {
+        $this->encounter = $encounter;
+        $this->encounterAttribute->addRecord($encounter);
+        return $this;
+    }
+
+    public function getEncounter() : ?Encounter
+    {
+        return $this->encounter;
     }
 }
