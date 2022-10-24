b053d0122d4f2490823cdde9d7ebdc170c2f7447
Mistralys <s.mordziol@gmail.com>
2022-10-24 18:29:13 +0200

    Added a list of known quests for easy referencing.

---
+++ src/CommonRecords/KnownQuests.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\FELHQuestEditor\CommonRecords;
+
+/**
+ * Constants for some bundled quests, to reference them easily.
+ */
+class KnownQuests
+{
+    public const QUEST_ESCORT_NOBLE_WOMAN = 'Quest_EscortNoblewoman';
+    public const QUEST_ESCORT_RAIDERS_BRIBE = 'Quest_TheRaidersBribe';
+    public const QUEST_ESCORT_SLAVERS_FARM_GIRL = 'Quest_Slavers';
+
+    public const QUEST_ARCTIC_WOLF_CLOAK = 'Quest_ArcticWolfCloak';
+    public const QUEST_EXILE_OF_ICE = 'Quest_ExileOfIce';
+    public const QUEST_POTION_OF_RESTORATION = 'Quest_Restoration';
+    public const QUEST_RATS_IN_THE_RUINS = 'Quest_RatsInTheRuins';
+    public const QUEST_HAITAN_STAFF_OF_BANISHING = 'Quest_Haitan';
+
+    public const QUEST_ALCHEMISTS_OFFER = 'Quest_AlchemistsOffer';
+    public const QUEST_ALCHEMY = 'Quest_Alchemy';
+    public const QUEST_BACCO = 'Quest_Bacco';
+    public const QUEST_MAUSOLOS = 'Quest_Mausolos';
+
+    public const QUEST_THE_PLAGUE = 'Quest_ThePlague';
+    public const QUEST_THREE_SONS = 'Quest_ThreeSons';
+    public const QUEST_CLOAK_OF_STARS = 'Quest_CloakOfStars';
+    public const QUEST_THE_OBSIDIAN_SHIELD = 'Quest_TheObsidianShield';
+}
