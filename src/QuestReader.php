d7b1b3b0971f0f2f757636411ef4050a9b62c647
Mistralys <s.mordziol@gmail.com>
2022-10-24 18:29:38 +0200

    Improved parsing.

--- src/QuestReader.php
+++ src/QuestReader.php
@@ -53,25 +53,24 @@ class QuestReader
         )
             ->toArray();
 
-        return $data['QuestDef'] ?? array();
+        return $data[Quest::TAG_NAME] ?? array();
     }
 
     private function parseQuest(array $questData, FileInfo $sourceFile) : void
     {
         $quest = new Quest($this->detectAttributes($questData), $sourceFile);
 
-        if(isset($questData['QuestObjectiveDef']))
+        if(isset($questData[QuestObjective::TAG_NAME]))
         {
-            foreach ($questData['QuestObjectiveDef'] as $objectiveDef)
+            $this->convertToList($questData, QuestObjective::TAG_NAME);
+
+            foreach ($questData[QuestObjective::TAG_NAME] as $objectiveDef)
             {
                 $this->parseObjective($quest, $objectiveDef);
             }
         }
 
-        if(isset($questData['Treasure']))
-        {
-            $this->parseTreasure($quest, $questData['Treasure']);
-        }
+        $this->processTreasure($questData, $quest);
 
         $this->quests->add($quest);
     }
@@ -80,48 +79,40 @@ class QuestReader
     {
         $objective = new QuestObjective($quest, $this->detectAttributes($objectiveData));
 
-        if(isset($objectiveData['QuestChoiceDef']))
+        if(isset($objectiveData[QuestChoice::TAG_NAME]))
         {
-            if(!isset($objectiveData['QuestChoiceDef'][0])) {
-                $objectiveData['QuestChoiceDef'] = array($objectiveData['QuestChoiceDef']);
-            }
+            $this->convertToList($objectiveData, QuestChoice::TAG_NAME);
 
-            foreach($objectiveData['QuestChoiceDef'] as $idx => $choiceData)
+            foreach($objectiveData[QuestChoice::TAG_NAME] as $idx => $choiceData)
             {
-                $choice = new QuestChoice($objective, $idx, $this->detectAttributes($choiceData));
-                $objective->addChoice($choice);
-
-                if(isset($choiceData['Encounter'])) {
-                    $this->parseEncounter($choice, $choiceData['Encounter']);
-                }
-            }
-
-            if(isset($questData['Treasure']))
-            {
-                $this->parseTreasure($objective, $questData['Treasure']);
+                $this->parseChoice($objective, $idx, $choiceData);
             }
         }
 
-        if(isset($objectiveData['QuestConditionDef']))
+        $this->processTreasure($objectiveData, $objective);
+
+        if(isset($objectiveData[QuestCondition::TAG_NAME]))
         {
-            if(!isset($objectiveData['QuestConditionDef'][0])) {
-                $objectiveData['QuestConditionDef'] = array($objectiveData['QuestConditionDef']);
-            }
+            $this->convertToList($objectiveData, QuestCondition::TAG_NAME);
 
-            foreach($objectiveData['QuestConditionDef'] as $conditionData)
+            foreach($objectiveData[QuestCondition::TAG_NAME] as $conditionData)
             {
-                $conditionDef = new QuestCondition($objective, $this->detectAttributes($conditionData));
-
-                if(isset($conditionData['Objective']))
-                {
-                    $conditionDef->setObjective(new Objective($this->detectAttributes($conditionData['Objective'])));
-                }
-
-                $objective->addCondition($conditionDef);
+                $this->parseCondition($objective, $conditionData);
             }
         }
 
         $quest->addObjective($objective);
+    }
+
+    private function processTreasure(array $data, TreasureContainerInterface $container) : void
+    {
+        if(!isset($objectiveData[Treasure::TAG_NAME]))
+        {
+            return;
+        }
+
+        $this->convertToList($data, Treasure::TAG_NAME);
+        $this->parseTreasure($container, $data[Treasure::TAG_NAME]);
     }
 
     private function detectAttributes(array $dataSet) : ArrayDataCollection
@@ -146,18 +137,20 @@ class QuestReader
         return ArrayDataCollection::create($attribs);
     }
 
+    /**
+     * @param TreasureContainerInterface $container
+     * @param array<int,array{GameModifier:array<string,mixed>}> $gameModifiers
+     * @return void
+     */
     private function parseTreasure(TreasureContainerInterface $container, array $gameModifiers) : void
     {
+        // Treasures do not have any attributes themselves:
+        // They are a collection of GameModifier tags.
         $treasure = new Treasure(new ArrayDataCollection(array()));
-
-        // Ensure it's a list
-        if(!isset($gameModifiers[0])) {
-            $gameModifiers = array($gameModifiers);
-        }
 
         foreach($gameModifiers as $gameModifier)
         {
-            $this->parseGameModifier($treasure, $gameModifier['GameModifier']);
+            $this->parseGameModifier($treasure, $gameModifier[GameModifier::TAG_NAME]);
         }
 
         $container->addTreasure($treasure);
@@ -176,19 +169,58 @@ class QuestReader
 
         $choice->setEncounter($encounter);
 
-        if(!isset($encounterData['UnitInstance']))
+        if(!isset($encounterData[UnitInstance::TAG_NAME]))
         {
             return;
         }
-        
-        if(!isset($encounterData['UnitInstance'][0])) {
-            $encounterData['UnitInstance'] = array($encounterData['UnitInstance']);
-        }
 
-        foreach($encounterData['UnitInstance'] as $unitData)
+        $this->convertToList($encounterData, UnitInstance::TAG_NAME);
+        
+        foreach($encounterData[UnitInstance::TAG_NAME] as $unitData)
         {
             $unit = new UnitInstance($encounter, $this->detectAttributes($unitData));
             $encounter->addUnitInstance($unit);
         }
+    }
+
+    /**
+     * Because of the way that the XMLHelper converts XML to
+     * JSON, nodes that only contain a single record will not
+     * be an indexed array of entries, even if they usually are.
+     *
+     * This ensures that the target data key is always a list.
+     *
+     * @param array<mixed> $dataSet
+     * @param string $tagName
+     * @return void
+     */
+    private function convertToList(array &$dataSet, string $tagName) : void
+    {
+        if(!isset($dataSet[$tagName][0])) {
+            $dataSet[$tagName] = array($dataSet[$tagName]);
+        }
+    }
+
+    private function parseChoice(QuestObjective $objective, int $number, array $choiceData) : void
+    {
+        $choice = new QuestChoice($objective, $number, $this->detectAttributes($choiceData));
+
+        $objective->addChoice($choice);
+
+        if(isset($choiceData[Encounter::TAG_NAME])) {
+            $this->parseEncounter($choice, $choiceData[Encounter::TAG_NAME]);
+        }
+    }
+
+    private function parseCondition(QuestObjective $objective, $conditionData) : void
+    {
+        $conditionDef = new QuestCondition($objective, $this->detectAttributes($conditionData));
+
+        if(isset($conditionData[Objective::TAG_NAME]))
+        {
+            $conditionDef->setObjective(new Objective($this->detectAttributes($conditionData[Objective::TAG_NAME])));
+        }
+
+        $objective->addCondition($conditionDef);
     }
 }
